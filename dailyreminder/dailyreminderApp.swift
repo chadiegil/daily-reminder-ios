735f80332788258415f957a9e0b243b2eccce451
Chadie Gil <chadiegil@gmail.com>
2026-02-06 16:43:55 +0800

    Initial Commit

---
+++ dailyreminder/dailyreminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  dailyreminderApp.swift
+//  dailyreminder
+//
+//  Created by Chadie Gil on 2/6/26.
+//
+
+import SwiftUI
+
+@main
+struct dailyreminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
